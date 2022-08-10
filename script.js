96599ee91ca4bfca02618251c28e1a20844c7cd4
player-number-three <wenhao_92@yahoo.com.sg>
2022-08-10 17:21:09 +0800

    ver 2 added make deck and shuffle deck functions

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ var main = function (input) {
 
 // no of players input
 var noOfPlayers = 0;
-var players = [];
+
 var noOfPlayersInput = function (input) {
   if (input == 0 || input == isNaN(input)) {
     console.log(`if input is not a valid number ${input}`);
@@ -23,7 +23,8 @@ var noOfPlayersInput = function (input) {
   }
   return myOutputValue;
 };
-
+// player cards array
+var players = [];
 // object the players name, cash capital
 var playerStats = function (noOfPlayers) {
   if (noOfPlayers != 0) {
@@ -32,7 +33,7 @@ var playerStats = function (noOfPlayers) {
     while (playersCounter <= noOfPlayers) {
       var playerNo = playersCounter;
       var playerCreation = {
-        name: "Player" + playerNo,
+        name: " Player " + playerNo,
         credits: 100,
         cardScore: "",
       };
@@ -47,13 +48,92 @@ var playerStats = function (noOfPlayers) {
       " Welcome Players, You will have $100 as a starting capital ! ";
   }
 };
-// player cards array
 
 // player score
 // no of players left after final cards calculation and cash capital
 
 // 2: cards deck
-// making of cards deck and shuffling
+// making of cards deck
+
+var makeDeck = function () {
+  // Initialise an empty deck array
+  var cardDeck = [];
+  // Initialise an array of the 4 suits in our deck. We will loop over this array.
+  var suits = ["hearts", "diamonds", "clubs", "spades"];
+
+  // Loop over the suits array
+  var suitIndex = 0;
+  while (suitIndex < suits.length) {
+    // Store the current suit in a variable
+    var currentSuit = suits[suitIndex];
+
+    // Loop from 1 to 13 to create all cards for a given suit
+    // Notice rankCounter starts at 1 and not 0, and ends at 13 and not 12.
+    // This is an example of a loop without an array.
+    var rankCounter = 1;
+    while (rankCounter <= 13) {
+      // By default, the card name is the same as rankCounter
+      var cardName = rankCounter;
+
+      // If rank is 1, 11, 12, or 13, set cardName to the ace or face card's name
+      if (cardName == 1) {
+        cardName = "ace";
+      } else if (cardName == 11) {
+        cardName = "jack";
+      } else if (cardName == 12) {
+        cardName = "queen";
+      } else if (cardName == 13) {
+        cardName = "king";
+      }
+
+      // Create a new card with the current name, suit, and rank
+      var card = {
+        name: cardName,
+        suit: currentSuit,
+        rank: rankCounter,
+      };
+
+      // Add the new card to the deck
+      cardDeck.push(card);
+
+      // Increment rankCounter to iterate over the next rank
+      rankCounter += 1;
+    }
+
+    // Increment the suit index to iterate over the next suit
+    suitIndex += 1;
+  }
+
+  // Return the completed card deck
+  return cardDeck;
+};
+var deck = makeDeck();
+// shuffling cards function
+// Get a random index ranging from 0 (inclusive) to max (exclusive).
+var getRandomIndex = function (max) {
+  return Math.floor(Math.random() * max);
+};
+
+// Shuffle the elements in the cardDeck array
+var shuffleCards = function (cardDeck) {
+  // Loop over the card deck array once
+  var currentIndex = 0;
+  while (currentIndex < cardDeck.length) {
+    // Select a random index in the deck
+    var randomIndex = getRandomIndex(cardDeck.length);
+    // Select the card that corresponds to randomIndex
+    var randomCard = cardDeck[randomIndex];
+    // Select the card that corresponds to currentIndex
+    var currentCard = cardDeck[currentIndex];
+    // Swap positions of randomCard and currentCard in the deck
+    cardDeck[currentIndex] = randomCard;
+    cardDeck[randomIndex] = currentCard;
+    // Increment currentIndex
+    currentIndex = currentIndex + 1;
+  }
+  // Return the shuffled deck
+  return cardDeck;
+};
 // after every round, cards are added back into the deck
 
 // 3: game logic
